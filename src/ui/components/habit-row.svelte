084472e7bf9ee4430a1d6e755828c6868617f1be
Lorenzo Dal'Aqua <lorenzodalaqua@gmail.com>
2020-10-08 01:05:52 -0300

    Made UI bigger on small screens and standardized styles in global.css.

--- src/ui/components/habit-row.svelte
+++ src/ui/components/habit-row.svelte
@@ -22,11 +22,11 @@
     'Which habit do you want to reinforce?'
   ];
   function getCheckboxClass(day) {
-    let className = '';
+    let className = 'checkbox';
     if (isCurrentMonth && day + 1 == today) {
-      className += 'today';
+      className += ' today';
     }
-    if (day > 0 && days[day] && days[day - 1]) {
+    if (day < days.length - 1 && days[day] && days[day + 1]) {
       className += ' streak';
     }
     return className;
@@ -44,37 +44,31 @@
 </script>
 
 <style>
-  :root {
-    --input-size: 1.8em;
+  .habit-color {
+    color: var(--habit-color);
   }
 
   .row {
     width: 100%;
     margin: 0;
     margin-bottom: 0.25em;
-    display: grid;
-    grid-template-columns: 1fr;
-    place-items: center;
-  }
-
-  .checkboxes {
     width: 100%;
     display: flex;
     flex-direction: row;
     align-items: center;
-    justify-content: center;
+    justify-content: flex-start;
     flex-wrap: wrap;
   }
 
-  label.checkbox {
+  .checkbox-label {
     position: relative;
   }
-  label.checkbox svg {
+  .checkbox-label svg {
     position: absolute;
     display: none;
   }
 
-  input[type='checkbox']:checked ~ svg {
+  .checkbox:checked ~ svg {
     position: absolute;
     display: block;
     top: 10px;
@@ -88,7 +82,7 @@
     fill: currentColor;
   }
 
-  input[type='checkbox'].today:checked ~ svg {
+  .checkbox.today:checked ~ svg {
     background: var(--habit-color);
     color: white;
     border-radius: 50%;
@@ -101,73 +95,7 @@
     padding: 1px;
   }
 
-  input[type='text'] {
-    min-width: 100%;
-    flex: 1 1 100%;
-  }
-
-  @media all and (min-width: 1000px) {
-    :root {
-      --input-size: 1.7em;
-    }
-
-    input[type='text'] {
-      min-width: 8em;
-      flex: 1 1 8em;
-    }
-  }
-
-  @media all and (min-width: 1500px) {
-    input[type='text'] {
-      min-width: 12em;
-      flex: 1 1 12em;
-    }
-  }
-
-  input {
-    border-radius: 8px;
-    border: 1px solid white;
-    background: #ffffff;
-    box-shadow: 1px 1px 4px #cccccc, -1px -1px 4px #ffffff;
-    margin: 0;
-    padding: 0;
-    margin: 5px;
-  }
-
-  input[type='color'] {
-    background: white;
-    width: var(--input-size);
-    height: var(--input-size);
-  }
-
-  input[type='text'] {
-    color: var(--habit-color);
-    font-weight: bold;
-    font-size: 0.9em;
-    padding: 0 0.2em;
-    height: var(--input-size);
-  }
-
-  input:hover {
-    border: 1px solid var(--habit-color);
-  }
-
-  input[type='checkbox'] {
-    -webkit-appearance: none;
-    appearance: none;
-    position: relative;
-    width: var(--input-size);
-    height: var(--input-size);
-    display: flex;
-    padding: 0;
-  }
-
-  input[type='checkbox']:checked {
-    color: var(--habit-color);
-    box-shadow: inset -1px -1px 6px #ffffff, inset 1px 1px 6px #cccccc;
-  }
-
-  input[type='checkbox']:before {
+  .checkbox:before {
     display: block;
     color: var(--text-color);
     content: attr(data-day);
@@ -179,116 +107,109 @@
     width: 100%;
     height: 100%;
   }
-  input[type='checkbox']:checked:before {
+  .checkbox:checked:before {
     display: none;
   }
 
-  input[type='checkbox'].today:not(:checked) {
+  .checkbox.today:not(:checked) {
     background-color: var(--habit-color);
     border: 1px solid var(--habit-color);
   }
 
-  input[type='checkbox'].today:not(:checked):focus {
+  .checkbox.today:not(:checked):focus {
     border: 1px solid var(--accent-color);
   }
 
-  input[type='checkbox'].today:not(:checked):before {
+  .checkbox.today:not(:checked):before {
     color: white;
   }
 
-  input[type='checkbox'].streak:after {
+  .checkbox.streak:after {
     content: ' ';
     position: absolute;
-    height: 3px;
-    width: 12px;
-    left: -13px;
-    top: calc(50% - 1px);
+    height: 5px;
+    width: calc(var(--input-size) / 3);
+    right: calc(var(--input-size) / -3 - 1px);
+    top: calc(50% - 2px);
     background: var(--habit-color);
   }
 
-  input:focus {
-    outline: none;
-    border: 1px solid var(--accent-color);
+  .habit-name-text {
+    flex: 1 1 100%;
   }
 
-  input[type='text']:focus {
-    box-shadow: inset -1px -1px 6px #ffffff, inset 1px 1px 6px #cccccc;
+  @media all and (min-width: 1000px) {
+    .habit-name-text {
+      min-width: 8em;
+      flex: 1 1 8em;
+    }
   }
 
-  .icon-button {
-    color: var(--habit-color);
-    border: 0px;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    padding: 5px;
-  }
-
-  .icon {
-    width: 1em;
-    height: 1em;
+  @media all and (min-width: 1500px) {
+    .habit-name-text {
+      min-width: 15em;
+      flex: 1 1 15em;
+    }
   }
 </style>
 
-<div class="list" style="--habit-color: {color}">
-  <div class="row">
-    <div class="checkboxes">
-      <label class="hidden" for={`name-${id}`}>Nome do hábito:</label>
-      <input
-        id={`name-${id}`}
-        type="text"
-        placeholder={placeholders[index % placeholders.length]}
-        value={name}
-        on:input={event => habitTrackerStore.setHabitName(id, event.target.value)} />
-      {#each days as value, day}
-        <label class="checkbox" for={`${day}-${id}`}>
-          <span class="hidden">{name} - {day + 1}/{month}/{year}</span>
-          <input
-            data-day={day + 1}
-            class={classes[day]}
-            id={`${day}-${id}`}
-            type="checkbox"
-            checked={value}
-            on:change={() => habitTrackerStore.toggleHabitDay(id, day, month, year)} />
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="25"
-            height="25"
-            viewBox="0 0 25 25"><path
-              d="M12.98 20.744a3.094 3.094 0 01-4.789.235l-5.575-6.176a2.389 2.389 0 01.176-3.377 2.398 2.398 0 013.383.176l3.944 4.37a.291.291 0 00.45-.023l8.108-10.976a2.398 2.398 0 013.35-.506 2.388 2.388 0 01.505 3.343z" /></svg>
-        </label>
-      {/each}
-      <label class="hidden" for={`${id}-color`}>Cor do hábito {name}</label>
-      <input
-        id={`${id}-color`}
-        type="color"
-        value={color}
-        on:change={event => habitTrackerStore.setHabitColor(id, event.target.value)} />
-      <button
-        on:click={deletePromptOpen ? closeDeletePrompt : openDeletePrompt}
-        class="icon-button"><span class="hidden">Delete</span>
+<div style="--habit-color: {color}">
+  <div class="row habit-color">
+    <label class="hidden" for={`name-${id}`}>Nome do hábito:</label>
+    <input
+      id={`name-${id}`}
+      type="text"
+      class="habit-name-text"
+      placeholder={placeholders[index % placeholders.length]}
+      value={name}
+      on:input={event => habitTrackerStore.setHabitName(id, event.target.value)} />
+    {#each days as value, day}
+      <label class="checkbox-label" for={`${day}-${id}`}>
+        <span class="hidden">{name} - {day + 1}/{month}/{year}</span>
+        <input
+          data-day={day + 1}
+          class={classes[day]}
+          id={`${day}-${id}`}
+          type="checkbox"
+          checked={value}
+          on:change={() => habitTrackerStore.toggleHabitDay(id, day, month, year)} />
         <svg
-          class="icon"
           xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"><title>Delete</title>
-          <path
-            d="M6 2l2-2h4l2 2h4v2H2V2h4zM3 6h14l-1 14H4L3 6zm5 2v10h1V8H8zm3 0v10h1V8h-1z" /></svg>
-      </button>
-    </div>
-    {#if deletePromptOpen}
-      <div>
-        <span>
-          Are you sure you want to delete? You will lose forever all data for
-          this habit.
-        </span>
-        <button on:click={closeDeletePrompt}>Cancel</button><button
-          on:click={() => {
-            deleteHabit(id);
-            closeDeletePrompt();
-          }}>Delete</button>
-      </div>
-    {/if}
+          width="25"
+          height="25"
+          viewBox="0 0 25 25"><path
+            d="M12.98 20.744a3.094 3.094 0 01-4.789.235l-5.575-6.176a2.389 2.389 0 01.176-3.377 2.398 2.398 0 013.383.176l3.944 4.37a.291.291 0 00.45-.023l8.108-10.976a2.398 2.398 0 013.35-.506 2.388 2.388 0 01.505 3.343z" /></svg>
+      </label>
+    {/each}
+    <label class="hidden" for={`${id}-color`}>Cor do hábito {name}</label>
+    <input
+      id={`${id}-color`}
+      type="color"
+      value={color}
+      on:change={event => habitTrackerStore.setHabitColor(id, event.target.value)} />
+    <button
+      on:click={deletePromptOpen ? closeDeletePrompt : openDeletePrompt}
+      class="icon-button delete-button"><span class="hidden">Delete</span>
+      <svg
+        class="icon"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"><title>Delete</title>
+        <path
+          d="M6 2l2-2h4l2 2h4v2H2V2h4zM3 6h14l-1 14H4L3 6zm5 2v10h1V8H8zm3 0v10h1V8h-1z" /></svg>
+    </button>
   </div>
+  {#if deletePromptOpen}
+    <div class="habit-color">
+      <span>
+        Are you sure you want to delete? You will lose forever all data for this
+        habit.
+      </span>
+      <button on:click={closeDeletePrompt}>Cancel</button><button
+        on:click={() => {
+          deleteHabit(id);
+          closeDeletePrompt();
+        }}>Delete</button>
+    </div>
+  {/if}
 </div>
--- src/ui/components/habit-tracker.svelte
+++ src/ui/components/habit-tracker.svelte
@@ -98,6 +98,11 @@
   div {
     margin-bottom: 0.5em;
   }
+
+  .controls {
+    color: var(--accent-color);
+  }
+
   .monthPicker {
     display: flex;
   }
@@ -105,9 +110,30 @@
   .monthPicker * {
     width: 100%;
   }
-  @media all and (min-width: 700px) {
+  @media all and (min-width: 768px) {
     .monthPicker {
-      max-width: 20em;
+      max-width: 20rem;
+    }
+  }
+
+  .add-habit {
+    display: flex;
+  }
+  .add-habit-button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex: 1 1 auto;
+  }
+  .add-habit-button svg {
+    margin-right: 0.5em;
+    fill: currentColor;
+    width: 1em;
+    height: 1em;
+  }
+  @media all and (min-width: 768px) {
+    .add-habit-button {
+      flex: 0 0 auto;
     }
   }
 </style>
@@ -134,7 +160,7 @@
       <a href="#login">Login</a>
     </div>
   {/if}
-  <div>
+  <div class="controls">
     <div class="monthPicker">
       <label class="hidden" for="month"> Month </label>
       <select bind:value={month} id="month">
@@ -152,6 +178,18 @@
     {#each Object.keys(tracker.habits) as id, index}
       <HabitRow {index} {id} {month} {year} />
     {/each}
-    <button on:click={() => habitTrackerStore.addHabit()}> Add habit </button>
+    <div class="add-habit">
+      <button
+        class="add-habit-button"
+        on:click={() => habitTrackerStore.addHabit()}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"><path
+            d="M19.602 7.67a1.316 1.316 0 0 0-.966-.397h-5.909v-5.91c0-.378-.133-.7-.398-.965A1.315 1.315 0 0 0 11.363 0H8.636c-.378 0-.7.132-.966.398a1.315 1.315 0 0 0-.397.966v5.909h-5.91c-.378 0-.7.132-.965.398A1.315 1.315 0 0 0 0 8.636v2.728c0 .379.132.7.398.966.265.265.587.397.966.397h5.909v5.91c0 .378.132.7.397.966.266.265.588.397.966.397h2.727c.38 0 .701-.132.966-.397.266-.266.398-.588.398-.966v-5.91h5.91c.378 0 .7-.132.965-.397s.398-.587.398-.966V8.636c0-.378-.132-.7-.398-.965z" /></svg>
+        Add habit
+      </button>
+    </div>
   </div>
 </div>
